5ccb4b2905ef5c24959392629c099aeb8a353d22
Ignat Stefan <ignatstefan21@stud.ase.ro>
2024-06-25 20:47:17 +0300

    reservation page configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import MyHotels from "./pages/MyHotels"; // Import the MyHotels component
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
+import Booking from "./pages/Booking";
 
 const App = () =>{
   const {isLoggedIn} = useAppContext();
@@ -33,6 +34,11 @@ const App = () =>{
 
         {isLoggedIn && (
           <>
+            <Route path="/hotel/:hotelId/booking" element={
+              <Layout>
+                <Booking />
+              </Layout>
+            } />
             <Route path="/add-hotel" element={
               <Layout>
                 <AddHotel />
