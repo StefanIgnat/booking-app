4bbc95daab5dfe9992973a57b07badf48469f6fe
Ignat Stefan <ignatstefan21@stud.ase.ro>
2024-06-25 23:25:36 +0300

    home page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 const App = () =>{
   const {isLoggedIn} = useAppContext();
@@ -24,7 +25,7 @@ const App = () =>{
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<Layout><p>Home Page</p></Layout>}/>
+        <Route path="/" element={<Layout><Home/></Layout>}/>
         <Route path="/search" element={<Layout><Search/></Layout>} />
         <Route path="/detail/:hotelId" element={<Layout><Detail/></Layout>} />
         <Route path = "/register" element={<Layout>
